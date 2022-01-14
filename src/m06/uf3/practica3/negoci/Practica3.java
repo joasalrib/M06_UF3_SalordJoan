ca3370d4bc34d2d703167495e2e6aa2398bcedb6
ausias <ausias@h5-17.xiam.cat>
2022-01-14 10:34:20 +0100

    Fixed "Controller-Model" view. Proper usage of interface.

--- src/m06/uf3/practica3/negoci/Practica3.java
+++ src/m06/uf3/practica3/negoci/Practica3.java
@@ -1,5 +1,6 @@
 package m06.uf3.practica3.negoci;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.basex.core.BaseXException;
@@ -10,79 +11,67 @@ import org.basex.core.cmd.Open;
 import org.basex.core.cmd.XQuery;
 import org.basex.query.QueryException;
 
+import m06.uf3.practica3.presentacio.Interficie;
+
 public class Practica3 {
+	private static Interficie interficie = new Interficie();
 	private static Context context = new Context();
 	private static String query;
 	
 	public static void main (String args[]) throws BaseXException, QueryException {
 		
-		System.out.println("#######################################################");
-		System.out.println("**********CONSULTING APP FOR \"Factbook.xml\"**********");
-		System.out.println("#######################################################");
+		interficie.presentation();
 		
-		//System.out.println("Obre DB");
-		//new CreateDB("factbook", "factbook.xml").execute(context);
 		new Open("factbook").execute(context);
-		//System.out.println(new InfoDB().execute(context));
 		System.out.println(new XQuery("for $pais in //country return data($pais/name)").execute(context));
 
-
 		getQuery();
-		
 
 		context.close();
 	}
 	
 	private static void getQuery() {
-		System.out.println("\nIntroduce your desired country | Or press 1 to see the country list again.");
+		interficie.showMenu();
 		Scanner sc = new Scanner(System.in);
 		query = sc.nextLine();
 		String a = query.substring(0,1);
 		query = query.substring(1, query.length());
 		query = a.toUpperCase() + query;
-		
-		
-		//System.out.println("input; " + query);
-		
-		//System.out.println(query.equals("1"));
-		
+
 		try {
-			if(query.equals("1")) {
-				//System.out.println("yes");
-				System.out.println(new XQuery("for $pais in //country return data($pais/name)").execute(context));
+			if(query.equals("1")) {				
+				interficie.showFullList(new XQuery("for $pais in //country return data($pais/name)").execute(context));
 				getQuery();
 			} else {
 				query(query);
 			}
 		} catch (BaseXException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (QueryException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		sc.close();
 	}
 	
 	private static void query(String query) throws QueryException, BaseXException{
-		//System.out.println("query");
 		
-		//
-		//context.close();
+		ArrayList<String> result = new ArrayList<String>();
 		
 		if (new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return $pais/name/text()").execute(context) == "") {
-			System.out.println("The country does not exist, try again.");
+			interficie.countryNotExist();
 			getQuery();
 		}
-		System.out.println("\n****This is your chosen country's data****");
-		System.out.println("Name: " + new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return $pais/name/text()").execute(context));
+		
+		result.add(query);
+		
+		result.add("Name: " + new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return $pais/name/text()").execute(context));
 		
 		if (new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return data($pais/@inflation)").execute(context) != "") {
-			System.out.println("La inflació és del: " + new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return data($pais/@inflation)").execute(context) + "%");
+			result.add("La inflació és del: " + new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return data($pais/@inflation)").execute(context) + "%");
 		}
-		System.out.println("The official religions of the country are: ");
-		System.out.println(new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return data($pais/religions)").execute(context));
-
+		result.add("The official religions of the country are: \n" + new XQuery("for $pais in //country where $pais/name=\"" + query + "\" return data($pais/religions)").execute(context));
+		interficie.showSelectedCountry(result);
+		
 		getQuery();
 		
 	}
--- src/m06/uf3/practica3/presentacio/Interficie.java
+++ src/m06/uf3/practica3/presentacio/Interficie.java
@@ -1,5 +1,30 @@
 package m06.uf3.practica3.presentacio;
 
+import java.util.ArrayList;
+
 public class Interficie {
 
+	public void presentation() {
+		System.out.println("#######################################################");
+		System.out.println("**********CONSULTING APP FOR \"Factbook.xml\"**********");
+		System.out.println("#######################################################");
+	}
+	
+	public void showFullList(String fullList) {
+		System.out.println(fullList);
+	}
+	
+	public void showMenu() {
+		System.out.println("\nIntroduce your desired country | Or press 1 to see the country list again.");
+	}
+	
+	public void countryNotExist() {
+		System.out.println("The country does not exist, try again.");
+	}
+	
+	public void showSelectedCountry(ArrayList<String> result) {
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println(result.get(i));
+		}
+	}
 }
